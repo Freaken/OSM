1fd34f7824e91e309fa1c92744746553350d8cf8
Mathias Svensson <freaken@freaken.dk>
2011-02-20 19:06:11 +0100

    Read and write syscalls should now 'just work'(TM)

--- buenos/proc/syscall.c
+++ buenos/proc/syscall.c
@@ -33,12 +33,60 @@
  * $Id: syscall.c,v 1.3 2004/01/13 11:10:05 ttakanen Exp $
  *
  */
+#include "drivers/polltty.h"
+#include "kernel/assert.h"
 #include "kernel/cswitch.h"
-#include "proc/syscall.h"
 #include "kernel/halt.h"
 #include "kernel/panic.h"
 #include "lib/libc.h"
-#include "kernel/assert.h"
+#include "proc/syscall.h"
+#include "proc/process.h"
+
+int _syscall_write(context_t *user_context) {
+    int file_handle = user_context->cpu_regs[MIPS_REGISTER_A1];
+    char *buffer = (char*) user_context->cpu_regs[MIPS_REGISTER_A2];
+    int length = user_context->cpu_regs[MIPS_REGISTER_A3];
+    process_table_t *process = process_get_current_process_entry();
+    gcd_t *file = &process->files[file_handle];
+
+    if(file_handle < 0 || file_handle >= CONFIG_MAX_FILEHANDLES)
+        return SYSCALL_ILLEGAL_ARGUMENT;
+
+    if(file->device == NULL)
+        return SYSCALL_NOT_OPEN;
+
+    if(file->write == NULL)
+        return SYSCALL_OPERATION_NOT_POSSIBLE;
+
+    /* This function _should_ also test if buffer in a legal memory area */
+
+    length = file->write(file, buffer, length);
+
+    return length;
+}
+
+int _syscall_read(context_t *user_context) {
+    int file_handle = user_context->cpu_regs[MIPS_REGISTER_A1];
+    char *buffer = (char*) user_context->cpu_regs[MIPS_REGISTER_A2];
+    int length = user_context->cpu_regs[MIPS_REGISTER_A3];
+    process_table_t *process = process_get_current_process_entry();
+    gcd_t *file = &process->files[file_handle];
+
+    if(file_handle < 0 || file_handle >= CONFIG_MAX_FILEHANDLES)
+        return SYSCALL_ILLEGAL_ARGUMENT;
+
+    if(file->device == NULL)
+        return SYSCALL_NOT_OPEN;
+
+    if(file->read == NULL)
+        return SYSCALL_OPERATION_NOT_POSSIBLE;
+
+    /* This function _should_ also test if buffer in a legal memory area */
+
+    length = file->read(file, buffer, length);
+
+    return length;
+}
 
 /**
  * Handle system calls. Interrupts are enabled when this function is
@@ -62,7 +110,30 @@ void syscall_handle(context_t *user_context)
     case SYSCALL_HALT:
         halt_kernel();
         break;
-    default: 
+
+    case SYSCALL_READ:
+        user_context->cpu_regs[MIPS_REGISTER_V0] = _syscall_read(user_context);
+        break;
+
+    case SYSCALL_WRITE:
+        user_context->cpu_regs[MIPS_REGISTER_V0] = _syscall_write(user_context);
+        break;
+
+    case SYSCALL_EXIT:
+        process_finish((int) user_context->cpu_regs[MIPS_REGISTER_A1]);
+        break;
+
+    case SYSCALL_EXEC:
+        user_context->cpu_regs[MIPS_REGISTER_V0] =
+            process_spawn((const char*) user_context->cpu_regs[MIPS_REGISTER_A1]);
+        break;
+
+    case SYSCALL_JOIN:
+        user_context->cpu_regs[MIPS_REGISTER_V0] =
+            process_join((int) user_context->cpu_regs[MIPS_REGISTER_A1]);
+        break;
+
+    default:
         KERNEL_PANIC("Unhandled system call\n");
     }
 
