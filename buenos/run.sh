25682cf6a4882a77b4897577ee01fe3f93110464
Mathias Svensson <freaken@freaken.dk>
2011-02-21 11:07:34 +0100

    run.sh now supports multiple files

--- buenos/run.sh
+++ buenos/run.sh
@@ -8,5 +8,12 @@ make
 make -C tests/
 rm store.file
 util/tfstool create store.file 2048 disk1
-util/tfstool write store.file tests/$1 $1
-yams buenos initprog=[disk1]$1
+
+MAINFILE=$1
+
+while (( "$#" )); do
+  util/tfstool write store.file tests/$1 $1
+  shift
+done
+
+yams buenos initprog=[disk1]$MAINFILE
