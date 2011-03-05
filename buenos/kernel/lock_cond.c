ffc1cf436e2d53051f591063dc27ea62aa705ba7
Thorbjørn S. Kaiser <lazareth@gmail.com>
2011-03-05 12:31:11 +0100

    bruger while istedet for if

--- buenos/kernel/lock_cond.c
+++ buenos/kernel/lock_cond.c
@@ -15,26 +15,20 @@ void lock_acquire(lock_t *lock) {
     intr_status = _interrupt_disable();
     spinlock_acquire(&lock->slock);
 
-    if(lock->locked) {
+    while(lock->locked) {
         sleepq_add(lock);
         spinlock_release(&lock->slock);
         thread_switch();
-    } else {
-        lock->locked = true;
-        spinlock_release(&lock-slock);
+        spinlock_acquire(&lock->slock);
     }
+
+    lock->locked = true;
+    spinlock_release(&lock-slock);
+
     _interrupt_set_state(intr_status);
 }
 
 void lock_release(lock_t *lock) {
-    interrupt_status_t intr_status;
-
-    intr_status = _interrupt_disable();
-    spinlock_acquire(&lock->slock);
-
     lock->locked = false;
     sleepq_wake(lock);
-
-    spinlock_release(&lock->slock);
-    _interrupt_set_state(intr_status);
 }
