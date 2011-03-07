ca5bcc8ee363774e4ca4bd0e1e896a2ae47f22e1
Thorbjørn S. Kaiser <lazareth@gmail.com>
2011-03-07 11:25:54 +0100

    implementeret opgave 2 (wait/signal)

--- buenos/kernel/lock_cond.c
+++ buenos/kernel/lock_cond.c
@@ -29,6 +29,39 @@ void lock_acquire(lock_t *lock) {
 }
 
 void lock_release(lock_t *lock) {
+    interrupt_status_t intr_status;
+
+    intr_status = _interrupt_disable();
+    spinlock_acquire(&lock->slock);
+
     lock->locked = false;
     sleepq_wake(lock);
+
+    spinlock_release(&lock->slock);
+    _interrupt_set_state(intr_status);
+}
+
+/* Useless! Literally no use. */
+int condition_reset(cond_t *cond) {
+    return 0;
+}
+
+void condition_wait(cond_t *cond, lock_t *condition_lock) {
+    interrupt_status_t intr_status;
+    intr_status = _interrupt_disable();
+    
+    sleepq_add(cond);
+    lock_release(condition_lock);
+    thread_switch();
+    lock_acquire(condition_lock);
+
+    _interrupt_set_state(intr_status);
+}
+
+void condition_signal(cond_t *cond) {
+    sleepq_wake(cond);
+}
+
+void condition_broadcast(cond_t *cond) {
+    sleepq_wake_all(cond);
 }
--- buenos/kernel/lock_cond.h
+++ buenos/kernel/lock_cond.h
@@ -9,3 +9,10 @@ typedef struct {
 int lock_reset(lock_t *lock);
 void lock_acquire(lock_t *lock);
 void lock_release(lock_t *lock);
+
+typedef struct { } cond_t;
+
+int condition_reset(cond_t *cond);
+void condition_wait(cond_t *cond, lock_t *condition_lock);
+void condition_signal(cond_t *cond);
+void condition_broadcast(cond_t *cond);
