ff828b7f0f8038f25d4dcf83a3f6b47da36a4ce9
Kristoffer Søholm <k.soeholm@gmail.com>
2011-03-14 16:14:14 +0100

    lille ændring i tests/file.c

--- buenos/tests/file.c
+++ buenos/tests/file.c
@@ -8,11 +8,12 @@ int main(void) {
 
     syscall_create("[disk1]lol", 50);
     filehandle = syscall_open("[disk1]lol");
-    syscall_write(filehandle, "LOLOL i trol u!!!", 10);
-    syscall_seek(filehandle, 3);
+    syscall_write(filehandle, "LOLOL i trol u!!!", 20);
+    syscall_seek(filehandle, 0);
     read = syscall_read(filehandle, buffer, 49);
     syscall_close(filehandle);
     syscall_delete("[disk1]lol");
+
 
     printf("buffer: %s, read: %d \n", buffer, read);
 
